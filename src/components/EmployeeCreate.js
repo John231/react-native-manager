6751ee579c524e64e120d91c1cb410a33bb7eeee
John231 <j_b_93@hotmail.co.uk>
2018-01-08 15:44:34 +0100

    Lesson 134 - Extending the employee create form

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { Card, CardSection, Input, Button } from "./common";
 
 class EmployeeCreate extends Component {
   render() {
     return (
-      <View>
-        <Text>{"Employee Form"}</Text>
-      </View>
+      <Card>
+        <CardSection>
+          <Input label="Name" placeholder="Jane" />
+        </CardSection>
+        <CardSection>
+          <Input label="Phone" placeholder="0123456789" />
+        </CardSection>
+        <CardSection />
+        <CardSection>
+          <Button>{"Save"}</Button>
+        </CardSection>
+      </Card>
     );
   }
 }
