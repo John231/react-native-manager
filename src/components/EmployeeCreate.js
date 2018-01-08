68e766d9fd77f7ec4376c6a7c9bbbd693ef71e1b
John231 <j_b_93@hotmail.co.uk>
2018-01-08 16:15:12 +0100

    Lesson 137 - Properties updated dynamically in create employee form

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+
 import { Card, CardSection, Input, Button } from "./common";
+import { employeeUpdate } from "../actions";
 
 class EmployeeCreate extends Component {
   render() {
+    const { name, phone } = this.props;
     return (
       <Card>
         <CardSection>
-          <Input label="Name" placeholder="Jane" />
+          <Input
+            label="Name"
+            placeholder="Jane"
+            value={name}
+            onChangeText={value =>
+              this.props.employeeUpdate({ prop: "name", value })
+            }
+          />
         </CardSection>
         <CardSection>
-          <Input label="Phone" placeholder="0123456789" />
+          <Input
+            label="Phone"
+            placeholder="0123456789"
+            value={phone}
+            onChangeText={value =>
+              this.props.employeeUpdate({ prop: "phone", value })
+            }
+          />
         </CardSection>
         <CardSection />
         <CardSection>
@@ -19,4 +37,10 @@ class EmployeeCreate extends Component {
     );
   }
 }
-export default EmployeeCreate;
+
+const mapStateToProps = state => {
+  const { name, phone, shift } = state.employeeForm;
+  return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import AuthReducer from "./AuthReducer";
+import EmployeeFormReduer from "./EmployeeFormReducer";
 
 export default combineReducers({
-  auth: AuthReducer
+  auth: AuthReducer,
+  employeeForm: EmployeeFormReduer
 });
