5cb717f9936134d09e288936b1073b61909d3f0e
John231 <j_b_93@hotmail.co.uk>
2018-01-08 15:37:52 +0100

    Lesson 133 - Added employee create form

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Scene, Router } from "react-native-router-flux";
+import { Scene, Router, Actions } from "react-native-router-flux";
 import LoginForm from "./components/LoginForm";
 import EmployeeList from "./components/EmployeeList";
+import EmployeeCreate from "./components/EmployeeCreate";
 
 const RouterComponent = () => {
   return (
@@ -17,8 +18,13 @@ const RouterComponent = () => {
           initial
           rightTitle="Add"
           onRight={() => {
-            console.log("Right!");
+            Actions.employeeCreate();
           }}
+        />
+        <Scene
+          key="employeeCreate"
+          title="Create Employee"
+          component={EmployeeCreate}
         />
       </Scene>
     </Router>
