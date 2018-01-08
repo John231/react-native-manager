006a5ed17d0d3e82481bae36af45a76435a75c74
John231 <j_b_93@hotmail.co.uk>
2018-01-08 11:03:40 +0100

    Lesson 110 - Recreating the login form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { View, Text } from "react-native";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducers from "./reducers";
+import LoginForm from "./components/LoginForm";
 
 class App extends Component {
   componentWillMount() {
@@ -22,7 +23,7 @@ class App extends Component {
     return (
       <Provider store={createStore(reducers)}>
         <View>
-          <Text>{"Hello"}</Text>
+          <LoginForm />
         </View>
       </Provider>
     );
---
+++ src/components/LoginForm.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { Card, CardSection, Input, Button } from "./common";
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <Input label="Email" placeholder="email@gmail.com" />
+        </CardSection>
+        <CardSection>
+          <Input label="Password" placeholder="password" secureTextEntry />
+        </CardSection>
+        <CardSection>
+          <Button>{"Login"}</Button>
+        </CardSection>
+      </Card>
+    );
+  }
+}
+export default LoginForm;
