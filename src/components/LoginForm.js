b0ea8138e05c19dd84c4158703dffcf85fa1d8a6
John231 <j_b_93@hotmail.co.uk>
2018-01-08 11:13:20 +0100

    Lesson 111 - Created action creator for email update

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,15 +2,23 @@ import React, { Component } from "react";
 import { Card, CardSection, Input, Button } from "./common";
 
 class LoginForm extends Component {
+  onEmailChange(text) {}
+
   render() {
     return (
       <Card>
         <CardSection>
-          <Input label="Email" placeholder="email@gmail.com" />
+          <Input
+            label="Email"
+            placeholder="email@gmail.com"
+            onChangeText={this.onEmailChange.bind(this)}
+          />
         </CardSection>
+
         <CardSection>
           <Input label="Password" placeholder="password" secureTextEntry />
         </CardSection>
+
         <CardSection>
           <Button>{"Login"}</Button>
         </CardSection>
