54c5cc1722b0f6a664b69d96f6574df987064c3c
John231 <j_b_93@hotmail.co.uk>
2018-01-09 10:30:12 +0100

    Lesson 148 - Dynamic data source building

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, ListView } from "react-native";
 import { connect } from "react-redux";
 import { employeesFetch } from "../actions";
 
 class EmployeeList extends Component {
   componentWillMount() {
     this.props.employeesFetch();
+    this.createDataSource(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    // nextProps are next set of props that this component
+    // will be rendered with
+    // this.props is the old set of props
+    this.createDataSource(nextProps);
+  }
+
+  createDataSource({ employees }) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+    this.dataSource = ds.cloneWithRows(employees);
   }
 
   render() {
