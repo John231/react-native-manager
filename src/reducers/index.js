4860a8b40f7b6a9cd2f2b53c3334a54ea33de45f
John231 <j_b_93@hotmail.co.uk>
2018-01-09 10:20:48 +0100

    Lesson 147 - Fetching employees from firebase

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import AuthReducer from "./AuthReducer";
 import EmployeeFormReduer from "./EmployeeFormReducer";
+import EmployeeReducer from "./EmployeeReducer";
 
 export default combineReducers({
   auth: AuthReducer,
-  employeeForm: EmployeeFormReduer
+  employeeForm: EmployeeFormReduer,
+  employees: EmployeeReducer
 });
