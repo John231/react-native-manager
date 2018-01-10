2223d191c32d8e0622a619f0bb9aa7b97db6e653
John231 <j_b_93@hotmail.co.uk>
2018-01-10 14:52:10 +0100

    Lesson 154 - Completed the Employee Edit implementation

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,20 +1,41 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import _ from "lodash";
 
 import EmployeeForm from "./EmployeeForm";
 import { Card, CardSection, Button } from "./common";
+import { employeeUpdate } from "../actions";
 
 class EmployeeEdit extends Component {
+  componentWillMount() {
+    //iterate over employee prop and update reducer
+    _.each(this.props.employee, (value, prop) => {
+      this.props.employeeUpdate({ prop, value });
+    });
+  }
+
+  onButtonPress() {
+    const { name, phone, shift } = this.props;
+    console.log("Name: %s, Phone: %s, Shift: %s", name, phone, shift);
+  }
+
   render() {
     return (
       <Card>
         <EmployeeForm />
         <CardSection>
-          <Button>{"Save Changes"}</Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
+            {"Save Changes"}
+          </Button>
         </CardSection>
       </Card>
     );
   }
 }
 
-export default connect()(EmployeeEdit);
+const mapStateToProps = state => {
+  const { name, phone, shift } = state.employeeForm;
+  return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeEdit);
